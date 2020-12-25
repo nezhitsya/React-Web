c23a1e3602556ff4636411ef2ef249b3ebf7a9d9
nezhitsya <huyn9295@naver.com>
2020-12-26 00:10:53 +0900

    yarn add node-sass sass-loader

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/navigation'
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
     </div>
   );
 }
 
 export default App;
+
+// yarn start
+// yarn add node-sass sass-loader
---
+++ react_blog/src/components/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+    <nav className="site-navigation">
+        <span>React Blog</span>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </nav>)
+}
