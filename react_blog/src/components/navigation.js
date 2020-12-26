eb3471924ddafda5106a7098dd853f0ca51940bc
nezhitsya <huyn9295@naver.com>
2020-12-27 00:24:51 +0900

    set up navigation

--- react_blog/src/components/navigation.js
+++ react_blog/src/components/navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { Avatar } from 'antd'
 
 const navLinks = [
     {
@@ -22,16 +23,18 @@ const navLinks = [
 
 export default function Navigation() {
     return (
-    <nav className="site-navigation">
+    <nav className="site-navigation" role="navigation">
         <span className="menu-title">React Blog</span>
-        <div>
+        <div className="menu-content-container">
             <ul>
                 { navLinks.map((link, index) => (
                     <li key={index}>
                         <Link to={link.path}>{link.title}</Link>
                     </li>
-                ))}
+                    ))
+                }
             </ul>
+            <Avatar src="http://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
         </div>
     </nav>)
 }
