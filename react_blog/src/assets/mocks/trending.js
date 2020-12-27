c6ce5e08ccd73c30e1c42d84b22a1a7406728fa3
nezhitsya <huyn9295@naver.com>
2020-12-27 23:57:46 +0900

    set up home

--- react_blog/src/assets/mocks/trending.js
+++ react_blog/src/assets/mocks/trending.js
@@ -8,7 +8,4 @@ export default [
         link: '#',
         image: '',
     },
-    {
-
-    },
 ]
--- react_blog/src/components/common/masonry-post.js
+++ react_blog/src/components/common/masonry-post.js
@@ -4,6 +4,7 @@ export default function MasonryPost({post, tagsOnTop}) {
     const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
 
     return (
-        <a className="masonry-post overlay" style={style} href={post.link} />
+        <a className="masonry-post overlay" style={style} href={post.link}>
+        </a>
     )
 }
