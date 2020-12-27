a3c5747fa25fc4c6c2cc49935b226245f4a15b73
nezhitsya <huyn9295@naver.com>
2020-12-28 00:14:40 +0900

    set up masonry style

--- react_blog/src/components/common/masonry-post.js
+++ react_blog/src/components/common/masonry-post.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import {categoryColors} from './styles'
 
 export default function MasonryPost({post, tagsOnTop}) {
     const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
 
     return (
         <a className="masonry-post overlay" style={style} href={post.link}>
-            <div className="image-text"></div>
+            <div className="image-text">
+                <div className="tags-container">
+                    { post.categories.map((tag, ind) =>
+                    <span key={ind} className="tag" style={{backgroundColor: categoryColors[tag]}}>
+                        {tag.toUpperCase()}
+                    </span>
+                    )}
+                </div>
+                <div>
+                    <h2 className="image-title">{post.title}</h2>
+                    <span className="image-date">{post.date}</span>
+                </div>
+            </div>
         </a>
     )
 }
---
+++ react_blog/src/components/common/styles.js
@@ -0,0 +1,12 @@
+const categoryColors = {
+    'Intro': 'rgb(255,59,48)',
+    'Tech News': 'rgb(0,113,164',
+    Vue: 'rgb(52,199,89',
+    React: 'rgb(64,156,255',
+    JavaScript: 'rgb(255,179,64',
+    Cloud: 'rgb(175,82,250'
+}
+
+export {
+    categoryColors
+}
