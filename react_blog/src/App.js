c64dc33f3c57c8a5c5443e3c02947ab57261fb51
nezhitsya <huyn9295@naver.com>
2021-01-04 15:23:25 +0900

    set up edit post

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -8,6 +8,8 @@ import Food from './pages/health'
 import Home from './pages/home'
 import Login from './pages/login'
 import ETC from './pages/web-dev'
+import EditPost from './pages/edit-post'
+import Post from './pages/post'
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Route path='/food' component={Food} />
           <Route path='/login' component={Login} />
           <Route path="/etc" component={ETC} />
+          <Route path="/edit-post" component={EditPost} />
+          <Route path='/post/:id' component={Post} />
           <Route path='/' component={Home} />
           <Route component={() => 404} />
         </Switch>
