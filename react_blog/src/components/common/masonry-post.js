59eafeebfae7e7cc5fb50278f32c6a630b7ae912
nezhitsya <huyn9295@naver.com>
2021-01-05 18:17:36 +0900

    set image url

--- react_blog/src/components/common/masonry-post.js
+++ react_blog/src/components/common/masonry-post.js
@@ -3,7 +3,8 @@ import {TagRow} from './'
 
 export default function MasonryPost ({post, tagsOnTop}) {
     const windowWidth = window.innerWidth
-    const imageBackground = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
+    // const imageBackground = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
+    const imageBackground = {backgroundImage: `url("${process.env.PUBLIC_URL}"+"${post.image}")`};
 
     const style = windowWidth > 900 ? {...imageBackground, ...post.style} : imageBackground
 
